6d1a2083c5e4065fcdeeab2150169e8bd605cc53
danielfry02 <djf17074@uga.edu>
2022-11-17 10:18:34 -0500

    Constructor added

--- src/Employee.java
+++ src/Employee.java
@@ -1,4 +1,21 @@
 
 public class Employee {
-private String Name = "Daniel";
+	protected String Name;
+	protected int Id;
+	protected int salary;
+	protected String level;
+	
+public Employee() {
+	Name= "addName";
+	Id= 0;
+	salary= 0;
+	level= "newHire";
+}
+	
+public Employee (String Name, int Id) {
+	this.Name=Name;
+	this.Id=Id;
+}
+
+
 }
